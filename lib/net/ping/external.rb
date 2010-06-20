bd28dd9edc70c288a5d11b727b404d89a9ef134b
Daniel Berger <djberg96@gmail.com>
2010-06-20 09:20:47 -0600

    Fixes to make Ping::External work with JRuby and some cosmetic changes.

--- lib/net/ping/external.rb
+++ lib/net/ping/external.rb
@@ -15,107 +15,112 @@ end
 # The Net module serves as a namespace only.
 module Net
 
-   # The Ping::External class encapsulates methods for external (system) pings.
-   class Ping::External < Ping
+  # The Ping::External class encapsulates methods for external (system) pings.
+  class Ping::External < Ping
+
+    CWINDOWS = Config::CONFIG['host_os'] =~ /mswin|win32|msdos|cygwin|mingw/i &&
+      RUBY_PLATFORM != 'java'
       
-      if Config::CONFIG['host_os'] =~ /mswin|win32|dos|cygwin|mingw/i
-         include Windows::Console
-      end 
+    if CWINDOWS
+      include Windows::Console
+    end 
    
-      # Pings the host using your system's ping utility and checks for any
-      # errors or warnings. Returns true if successful, or false if not.
-      # 
-      # If the ping failed then the Ping::External#exception method should
-      # contain a string indicating what went wrong. If the ping succeeded then
-      # the Ping::External#warning method may or may not contain a value.
-      # 
-      def ping(host = @host)
-         super(host)
+    # Pings the host using your system's ping utility and checks for any
+    # errors or warnings. Returns true if successful, or false if not.
+    # 
+    # If the ping failed then the Ping::External#exception method should
+    # contain a string indicating what went wrong. If the ping succeeded then
+    # the Ping::External#warning method may or may not contain a value.
+    # 
+    def ping(host = @host)
+      super(host)
 
-         input, output, error = ""
-         pstring = "ping "
-         bool    = false
-         orig_cp = nil
-         
-         case Config::CONFIG['host_os']
-            when /linux|bsd|osx|mach|darwin/i
-               pstring += "-c 1 #{host}"
-            when /solaris|sunos/i
-               pstring += "#{host} 1"
-            when /hpux/i
-               pstring += "#{host} -n 1"
-            when /win32|windows|mswin/i
-               orig_cp = GetConsoleCP()
-               SetConsoleCP(437) if orig_cp != 437 # United States
-               pstring += "-n 1 #{host}"
-            else
-               pstring += "#{host}"
-         end
-         
-         start_time = Time.now
-         
-         begin
-           e = nil
+      input, output, error = ""
+      pstring = "ping "
+      bool    = false
+      orig_cp = nil
+       
+      case Config::CONFIG['host_os']
+        when /linux|bsd|osx|mach|darwin/i
+          pstring += "-c 1 #{host}"
+        when /solaris|sunos/i
+          pstring += "#{host} 1"
+        when /hpux/i
+          pstring += "#{host} -n 1"
+        when /win32|windows|msdos|mswin|cygwin|mingw/i
+          if RUBY_PLATFORM != 'java'
+            orig_cp = GetConsoleCP()
+            SetConsoleCP(437) if orig_cp != 437 # United States
+          end
+          pstring += "-n 1 #{host}"
+        else
+          pstring += "#{host}"
+      end
+       
+      start_time = Time.now
+       
+      begin
+        err = nil
 
-           Timeout.timeout(@timeout){
-              input, output, error = Open3.popen3(pstring)
-              e = error.gets # Can't chomp yet, might be nil
-           }
+        Timeout.timeout(@timeout){
+          input, output, error = Open3.popen3(pstring)
+          err = error.gets # Can't chomp yet, might be nil
+        }
 
-           input.close
-           error.close
+        input.close
+        error.close
 
-           if Config::CONFIG['host_os'] =~ /mswin|win32|dos/i &&
-              GetConsoleCP() != orig_cp
-           then
-              SetConsoleCP(orig_cp)
-           end
-        
-           unless e.nil?
-              if e =~ /warning/i
-                 @warning = e.chomp
-                 bool = true
-              else
-                 @exception = e.chomp
+        if CWINDOWS && GetConsoleCP() != orig_cp
+          SetConsoleCP(orig_cp)
+        end
+      
+        unless err.nil?
+          if err =~ /warning/i
+            @warning = err.chomp
+            bool = true
+          else
+            @exception = err.chomp
+          end
+        # The "no answer" response goes to stdout, not stderr, so check it
+        else
+          lines = output.readlines
+          output.close
+          if lines.nil? || lines.empty?
+            bool = true
+          else
+            regexp = /
+              no\ answer|
+              host\ unreachable|
+              could\ not\ find\ host|
+              request\ timed\ out|
+              100%\ packet\ loss
+            /ix
+
+            lines.each{ |line|
+              if regexp.match(line)
+                @exception = line.chomp
+                break
               end
-           # The "no answer" response goes to stdout, not stderr, so check it
-           else
-              lines = output.readlines
-              output.close
-              if lines.nil? || lines.empty?
-                 bool = true
-              else
-                 regexp = /
-                    no\ answer|
-                    host\ unreachable|
-                    could\ not\ find\ host|
-                    request\ timed\ out|
-                    100%\ packet\ loss
-                 /ix
-                 lines.each{ |line|
-                    if regexp.match(line)
-                       @exception = line.chomp
-                       break
-                    end
-                 }
-                 bool = true unless @exception
-              end
-           end
-         rescue Exception => err
-           @exception = err.message 
-         ensure
-           input.close if input && !input.closed?
-           error.close if error && !error.closed?
-           output.close if output && !output.closed?
-         end
+            }
 
-         # There is no duration if the ping failed
-         @duration = Time.now - start_time if bool
-
-         bool
+            bool = true unless @exception
+          end
+        end
+      rescue Exception => error
+        @exception = error.message 
+      ensure
+        input.close if input && !input.closed?
+        error.close if error && !error.closed?
+        output.close if output && !output.closed?
       end
 
-      alias ping? ping
-      alias pingecho ping
-   end
+      # There is no duration if the ping failed
+      @duration = Time.now - start_time if bool
+
+      bool
+    end
+
+    alias ping? ping
+    alias pingecho ping
+  end
 end
