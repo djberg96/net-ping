01f8ac7fe688b0582676e4113d5fc1ff5453b70e
Paul Gallagher <gallagher.paul@gmail.com>
2011-04-30 18:17:32 +0800

    Update Ping::LDAP documentation
    * removed Gemfile from the project

--- lib/net/ping/ldap.rb
+++ lib/net/ping/ldap.rb
@@ -9,22 +9,43 @@ module Net
   # The Ping::LDAP class encapsulates methods for LDAP pings.
   class Ping::LDAP < Ping
 
+    # uri contains the URI object for the request
+    #
     attr_accessor :uri
+
+    # username and password may be set for ping using
+    # an authenticated LDAP bind
+    #
     attr_accessor :username
     attr_accessor :password
+
+    # set/get the encryption method. By default nil,
+    # but may be set to :simple_tls
+    #
     attr_accessor :encryption
     def encryption=(value)
       @encryption = (value.is_a? Symbol) ? value : value.to_sym
     end
 
-    # Creates and returns a new Ping::LDAP object. The default port is the
-    # port associated with the URI. The default timeout is 5 seconds.
+    # Creates and returns a new Ping::LDAP object.
+    # The default +timeout+ is 5 seconds.
     #
-    def initialize(host=nil, timeout=5)
-      host, port = decode_uri(host)
+    # +uri+ string is expected to be a full URI with scheme (ldap/ldaps)
+    # and optionally the port (else default port is assumed) e.g.
+    #   ldap://my.ldap.host.com
+    #   ldap://my.ldap.host.com:1389
+    #   ldaps://my.ldap.host.com
+    #   ldaps://my.ldap.host.com:6636
+    #
+    # If a plain hostname is provided as the +uri+, a default port of 389 is assumed
+    #
+    def initialize(uri=nil, timeout=5)
+      host, port = decode_uri(uri)
       super(host, port, timeout)
     end
 
+    # method used to decode uri string
+    #
     def decode_uri(value)
       @uri = URI.parse(value)
       if uri.scheme =~ /ldap/
@@ -38,20 +59,21 @@ module Net
       [h, p]
     end
 
+    # constructs the LDAP configuration structure
+    #
     def config
-      conf = {
+      {
         :host => uri.host,
         :port => uri.port,
         :encryption => encryption
-      }
-      conf.merge!(
+      }.merge(
         (username && password) ?
         { :auth => {:method => :simple, :username => username, :password => password} } :
         { :auth => {:method => :anonymous} }
       )
-      conf
     end
 
+    # perform ping, optionally providing the ping destination uri
     #
     def ping(host = nil)
       decode_uri(host) if host
