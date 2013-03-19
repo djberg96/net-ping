988dfa61966cc129623059ec518c1be5f15158bc
Daniel Berger <djberg96@gmail.com>
2013-03-19 06:40:57 -0600

    Version bumps, updates to the CHANGES file.

--- test/test_net_ping.rb
+++ test/test_net_ping.rb
@@ -28,7 +28,7 @@ end
 
 class TC_Net_Ping < Test::Unit::TestCase
   def test_net_ping_version
-    assert_equal('1.5.3', Net::Ping::VERSION)
+    assert_equal('1.6.0', Net::Ping::VERSION)
   end
 end
 
